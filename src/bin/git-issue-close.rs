e6f33848ec048ec305a15242941baf89d4ad82c8
Bahtiar `kalkin` Gadimov <bahtiar@gadimov.de>
2022-02-27 20:44:21 +0100

    refactor: Simplify `close` command

--- src/bin/git-issue-close.rs
+++ src/bin/git-issue-close.rs
@@ -45,10 +45,49 @@ fn close_issues(data: &DataSource, ids: &[Id]) -> Result<(), PosixError> {
     Ok(())
 }
 
+fn execute(args: &Args, mut data: DataSource) -> Result<(), PosixError> {
+    let issue_ids: Vec<Id> = args
+        .issue_ids
+        .iter()
+        .map(|id| data.find_issue(id))
+        .collect::<Result<Vec<Id>, FindError>>()
+        .map_err(PosixError::from)?;
+
+    log::info!("Starting transaction");
+    data.start_transaction().map_err(PosixError::from)?;
+
+    match close_issues(&data, &issue_ids) {
+        Ok(_) => {
+            let msg = if issue_ids.len() == 1 {
+                format!(
+                    "DONE({}): {}",
+                    &issue_ids[0].0[..8],
+                    data.title(&issue_ids[0]).unwrap()
+                )
+            } else {
+                let text = issue_ids
+                    .iter()
+                    .map(|id| &id.0[..8])
+                    .collect::<Vec<_>>()
+                    .join(", ");
+                format!("gi: Closed {}", text)
+            };
+            log::info!("Committing transaction");
+            data.finish_transaction(&msg).map_err(PosixError::from)
+        }
+        Err(e) => {
+            log::warn!("An error happend. Rolling back transaction.");
+            data.rollback_transaction()?;
+            Err(e)
+        }
+    }
+}
+
+#[cfg(not(tarpaulin_include))]
 fn main() {
     let args = Args::parse();
     set_log_level(&args);
-    let mut data = match git_issue::DataSource::try_new(&args.git_dir, &args.work_tree) {
+    let data = match git_issue::DataSource::try_new(&args.git_dir, &args.work_tree) {
         Err(e) => {
             log::error!(" error: {}", e);
             std::process::exit(128);
@@ -56,53 +95,7 @@ fn main() {
         Ok(d) => d,
     };
 
-    let issue_ids: Vec<Id> = match args
-        .issue_ids
-        .iter()
-        .map(|id| data.find_issue(id))
-        .collect::<Result<Vec<Id>, FindError>>()
-        .map_err(PosixError::from)
-    {
-        Err(e) => {
-            log::error!("{}", e);
-            std::process::exit(e.code());
-        }
-        Ok(ids) => ids,
-    };
-
-    log::info!("Starting transaction");
-    if let Err(e) = data.start_transaction().map_err(PosixError::from) {
-        log::error!("{}", e);
-        std::process::exit(e.code());
-    }
-
-    if let Err(e) = close_issues(&data, &issue_ids) {
-        log::error!("{}", e);
-        log::warn!("Rolling back transaction");
-        if let Err(err) = data.rollback_transaction().map_err(PosixError::from) {
-            log::error!("{}", err);
-            std::process::exit(err.code());
-        }
-        std::process::exit(e.code());
-    }
-
-    let msg = if issue_ids.len() == 1 {
-        format!(
-            "DONE({}): {}",
-            &issue_ids[0].0[..8],
-            data.title(&issue_ids[0]).unwrap()
-        )
-    } else {
-        let text = issue_ids
-            .iter()
-            .map(|id| &id.0[..8])
-            .collect::<Vec<_>>()
-            .join(", ");
-        format!("gi: Closed {}", text)
-    };
-
-    log::info!("Commiting transaction");
-    if let Err(e) = data.finish_transaction(&msg).map_err(PosixError::from) {
+    if let Err(e) = execute(&args, data) {
         log::error!("{}", e);
         std::process::exit(e.code());
     }
