9df66b384f4b8c971c4db814da78a31145a6200d
Bahtiar `kalkin` Gadimov <bahtiar@gadimov.de>
2022-03-29 19:15:40 +0200

    change(milestone): Use subcommands instead of switches

--- src/bin/git-issue-milestone.rs
+++ src/bin/git-issue-milestone.rs
@@ -1,33 +1,37 @@
 #![allow(missing_docs)]
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use clap_verbosity_flag::{Verbosity, WarnLevel};
 
 use posix_errors::PosixError;
 
 use git_issue::{DataSource, WriteResult};
 
+#[derive(Subcommand)]
+enum Command {
+    Remove {
+        /// Issue id
+        issue_id: String,
+    },
+    Set {
+        /// Issue id
+        issue_id: String,
+
+        /// Milestone name
+        milestone: String,
+    },
+}
+
+/// Manage milestones
 #[derive(Parser)]
 #[clap(
     author,
     version,
-    about = "Set or remove milestone",
     help_expected = true,
     dont_collapse_args_in_usage = true
 )]
 struct Args {
-    #[clap(long_help = "Issue id")]
-    issue_id: String,
-    #[clap(
-        short,
-        long,
-        long_help = "Remove milestone from issue",
-        conflicts_with = "milestone"
-    )]
-    remove: bool,
-
-    /// Milestone to set
-    #[clap(conflicts_with = "remove", required_unless_present = "remove")]
-    milestone: Option<String>,
+    #[clap(subcommand)]
+    command: Command,
 
     #[clap(long, long_help = "Directory where the GIT_DIR is")]
     git_dir: Option<String>,
@@ -38,75 +42,58 @@ struct Args {
     verbose: Verbosity<WarnLevel>,
 }
 
-fn execute(args: &Args, mut data: DataSource) -> Result<(), PosixError> {
-    let id = data.find_issue(&args.issue_id).map_err(PosixError::from)?;
+fn set_cmd(mut data: DataSource, issue_id: &str, milestone: &str) -> Result<(), PosixError> {
+    let id = data.find_issue(issue_id).map_err(PosixError::from)?;
     log::info!("Starting transaction");
     data.start_transaction().map_err(PosixError::from)?;
-    if args.remove {
-        match data.remove_milestone(&id) {
-            Err(e) => {
-                log::error!("{}", e);
-                log::info!("Rolling back transaction");
-                data.rollback_transaction().map_err(PosixError::from)?;
-                Err(PosixError::from(e))
-            }
-            Ok(WriteResult::NoChanges) => {
-                log::warn!("Issue already has no milestone {}", &id.short_id());
-                log::info!("Rolling back transaction");
-                data.rollback_transaction().map_err(PosixError::from)
-            }
-            Ok(WriteResult::Applied) => {
-                log::warn!("Removed milestone from issue {}", &id.short_id());
-                log::info!("Committing transaction");
-                data.finish_transaction_without_merge()
-                    .map_err(PosixError::from)
-            }
+
+    match data.add_milestone(&id, milestone) {
+        Err(e) => {
+            log::error!("{}", e);
+            log::info!("Rolling back transaction");
+            data.rollback_transaction().map_err(PosixError::from)?;
+            Err(PosixError::from(e))
         }
-    } else {
-        match data.add_milestone(&id, args.milestone.as_ref().expect("Expect milestone here")) {
-            Err(e) => {
-                log::error!("{}", e);
-                log::info!("Rolling back transaction");
-                data.rollback_transaction().map_err(PosixError::from)?;
-                Err(PosixError::from(e))
-            }
-            Ok(WriteResult::NoChanges) => {
-                log::warn!(
-                    "Milestone “{}” already set on issue {}",
-                    &args.milestone.as_ref().expect("Milestone set"),
-                    &id.short_id()
-                );
-                log::info!("Rolling back transaction");
-                data.rollback_transaction().map_err(PosixError::from)
-            }
-            Ok(WriteResult::Applied) => {
-                log::warn!(
-                    "Set milestone “{}” on issue {}",
-                    &args.milestone.as_ref().expect("Milestone set"),
-                    &id.short_id()
-                );
-                log::info!("Committing transaction");
-                data.finish_transaction_without_merge()
-                    .map_err(PosixError::from)
-            }
+        Ok(WriteResult::NoChanges) => {
+            log::warn!(
+                "Milestone “{}” already set on issue {}",
+                milestone,
+                &id.short_id()
+            );
+            log::info!("Rolling back transaction");
+            data.rollback_transaction().map_err(PosixError::from)
+        }
+        Ok(WriteResult::Applied) => {
+            log::warn!("Set milestone “{}” on issue {}", milestone, &id.short_id());
+            log::info!("Committing transaction");
+            data.finish_transaction_without_merge()
+                .map_err(PosixError::from)
         }
     }
+}
 
-    /*.map_err(PosixError::from)
-    .map_err(|e| {
-        })
-    .and_then(|r| match r {
-            WriteResult::Applied => {
-                log::warn!("Set milestone “{}” on issue {}", &args.milestone, &id.short_id());
-                log::info!("Committing transaction");
-                data.finish_transaction_without_merge().map_err(PosixError::from)
-            }
-            WriteResult::NoChanges => {
-                log::warn!("Milestone “{}” already set on issue {}", &args.milestone, &id.short_id());
-                log::info!("Rolling back transaction");
-                data.rollback_transaction().map_err(PosixError::from)
-            }
-        })*/
+fn remove_cmd(mut data: DataSource, issue_id: &str) -> Result<(), PosixError> {
+    let id = data.find_issue(issue_id).map_err(PosixError::from)?;
+    log::info!("Starting transaction");
+    match data.remove_milestone(&id) {
+        Err(e) => {
+            log::error!("{}", e);
+            log::info!("Rolling back transaction");
+            data.rollback_transaction().map_err(PosixError::from)?;
+            Err(PosixError::from(e))
+        }
+        Ok(WriteResult::NoChanges) => {
+            log::warn!("Issue already has no milestone {}", &id.short_id());
+            log::info!("Rolling back transaction");
+            data.rollback_transaction().map_err(PosixError::from)
+        }
+        Ok(WriteResult::Applied) => {
+            log::warn!("Removed milestone from issue {}", &id.short_id());
+            log::info!("Committing transaction");
+            data.finish_transaction_without_merge()
+                .map_err(PosixError::from)
+        }
+    }
 }
 
 #[cfg(not(tarpaulin_include))]
@@ -123,7 +110,13 @@ fn main() {
         Ok(repo) => repo,
     };
 
-    if let Err(e) = execute(&args, data) {
+    if let Err(e) = match args.command {
+        Command::Remove { issue_id } => remove_cmd(data, &issue_id),
+        Command::Set {
+            issue_id,
+            milestone,
+        } => set_cmd(data, &issue_id, &milestone),
+    } {
         log::error!("{}", e);
         std::process::exit(e.code());
     }
